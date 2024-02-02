f12bd378fd1b3fca76bf7984a33fcab80b85d6b6
Marouan Doulahiane <mdoulahi@e1r3p13.1337.ma>
2024-02-02 22:24:29 +0100

    by cypher

--- rendering/renderer.c
+++ rendering/renderer.c
@@ -6,7 +6,7 @@
 /*   By: mdoulahi <mdoulahi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 06:44:45 by mdoulahi          #+#    #+#             */
-/*   Updated: 2024/02/02 16:40:19 by mdoulahi         ###   ########.fr       */
+/*   Updated: 2024/02/02 22:23:21 by mdoulahi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,6 +50,14 @@ void	draw_circle(t_env *e, int radius)
 		i++;
 	}
 }
+// e->map[y / SIZE][x / SIZE] != '1' && e->map[y / SIZE][x / SIZE] != ' ' &&  (e->map[e->y / SIZE][x / SIZE] != '1' && e->map[y / SIZE][e->x / SIZE] != '1')
+bool	can_move(t_env *e, int x, int y)
+{
+	if (e->map[y / SIZE][x / SIZE] != '1' && e->map[y / SIZE][x / SIZE] != ' ' &&  (e->map[e->y / SIZE][x / SIZE] != '1' && e->map[y / SIZE][e->x / SIZE] != '1'))
+		return (true);
+	return (false);
+}
+
 
 void	move_up(t_env *e)
 {
@@ -58,7 +66,7 @@ void	move_up(t_env *e)
 	
 	x = e->x + cos(e->angle) * 5;
 	y = e->y + sin(e->angle) * 5;
-	if (e->map[y / SIZE][x / SIZE] != '1')
+	if (can_move(e, x, y))
 	{
 		e->x = x;
 		e->y = y;
@@ -72,7 +80,7 @@ void	move_down(t_env *e)
 	
 	x = e->x - cos(e->angle) * 5;
 	y = e->y - sin(e->angle) * 5;
-	if (e->map[y / SIZE][x / SIZE] != '1')
+	if (can_move(e, x, y))
 	{
 		e->x = x;
 		e->y = y;
@@ -84,9 +92,23 @@ void	move_right(t_env *e)
 	int	x;
 	int	y;
 	
+	x = e->x - sin(e->angle) * 5;
+	y = e->y + cos(e->angle) * 5;
+	if (can_move(e, x, y))
+	{
+		e->x = x;
+		e->y = y;
+	}
+}
+
+void	move_left(t_env *e)
+{
+	int	x;
+	int	y;
+	
 	x = e->x + sin(e->angle) * 5;
 	y = e->y - cos(e->angle) * 5;
-	if (e->map[y / SIZE][x / SIZE] != '1')
+	if (can_move(e, x, y))
 	{
 		e->x = x;
 		e->y = y;
@@ -112,12 +134,9 @@ void	movement(void *param)
 	if (mlx_is_key_down(e->mlx, MLX_KEY_S))
 		move_down(e);
 	if (mlx_is_key_down(e->mlx, MLX_KEY_A))
-		
+		move_left(e);
 	if (mlx_is_key_down(e->mlx, MLX_KEY_D))
-	{
-		x = e->x - sin(e->angle) * 5;
-		y = e->y + cos(e->angle) * 5;
-	}
+		move_right(e);
 	if (mlx_is_key_down(e->mlx, MLX_KEY_LEFT))
 	{
 		e->angle -= 0.1;
